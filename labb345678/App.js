95f2af83063c262609a7fa2f3cbf051abd5cd04b
congnguyen2005 <dangcongnguyenst@gmail.com>
2025-01-18 21:41:19 +0700

    add labb345678

--- labb345678/App.js
+++ labb345678/App.js
@@ -1,20 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import react from "react";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import Lab3 from "./lab3bai3";
+const App = () => {
+  return <Lab3></Lab3>;
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
